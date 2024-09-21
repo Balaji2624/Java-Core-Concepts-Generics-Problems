e9b363ca3ea03bd9227a39a5a59029f9250b105c
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-21 09:35:15 +0530

    Added: Array of Integer,double and character print array.

--- src/main/java/com/bridgelabz/genericsproblems/PrintArray.java
+++ src/main/java/com/bridgelabz/genericsproblems/PrintArray.java
@@ -2,7 +2,6 @@ package com.bridgelabz.genericsproblems;
 
 class PrintArray {
 
-    // Method to print an Integer array
     public static void toPrint(Integer[] array) {
         for (Integer element : array) {
             System.out.print(element + " ");
@@ -10,7 +9,6 @@ class PrintArray {
         System.out.println();
     }
 
-    // Method to print a Double array
     public static void toPrint(Double[] array) {
         for (Double element : array) {
             System.out.print(element + " ");
@@ -18,7 +16,6 @@ class PrintArray {
         System.out.println();
     }
 
-    // Method to print a Character array
     public static void toPrint(Character[] array) {
         for (Character element : array) {
             System.out.print(element + " ");
@@ -27,12 +24,11 @@ class PrintArray {
     }
 
     public static void main(String[] args) {
-        // Arrays of Integer, Double, and Character types
+
         Integer[] intArray = {1, 2, 3, 4, 5};
         Double[] doubleArray = {1.1, 2.2, 3.3, 4.4, 5.5};
         Character[] charArray = {'A', 'B', 'C', 'D', 'E'};
-
-        // Printing the arrays using the overloaded toPrint methods
+        
         System.out.println("Integer Array:");
         toPrint(intArray);
 
