c834ff693d906a3b5ae7b24a49d052e74c77c0dd
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-21 21:15:48 +0530

    Added: Extend the max method to also print the max using generics method.

--- src/main/java/com/bridgelabz/genericsproblems/MaxInteger.java
+++ src/main/java/com/bridgelabz/genericsproblems/MaxInteger.java
@@ -1,37 +1,49 @@
 package com.bridgelabz.genericsproblems;
 
 class MaxInteger<T extends Comparable<T>> {
-    private T[] elements;
 
-    // Parameterized constructor to initialize the generic array
-    @SafeVarargs
-    public MaxInteger(T... elements) {
-        this.elements = elements;
-    }
+        private T[] elements;
 
-    // Instance method to call the static testMaximum method
-    public T testMaximum() {
-        return MaxInteger.testMaximum(elements);
-    }
-
-    // Static generic method to find the maximum of any number of Comparable objects
-    @SafeVarargs
-    public static <T extends Comparable<T>> T testMaximum(T... elements) {
-        if (elements.length == 0) {
-            throw new IllegalArgumentException("No elements provided");
+        // Parameterized constructor to initialize the generic array
+        @SafeVarargs
+        public MaxInteger(T... elements) {
+            this.elements = elements;
         }
 
-        T max = elements[0]; // Initialize max with the first element
+        // Instance method to call the static testMaximum method
+        public T testMaximum() {
+            T max = MaxInteger.testMaximum(elements);
+            printMax(elements, max);
+            return max;
+        }
 
-        // Loop through all elements to find the maximum
-        for (T element : elements) {
-            if (element.compareTo(max) > 0) {
-                max = element;
+        // Static generic method to find the maximum of any number of Comparable objects
+        @SafeVarargs
+        public static <T extends Comparable<T>> T testMaximum(T... elements) {
+            if (elements.length == 0) {
+                throw new IllegalArgumentException("No elements provided");
             }
+
+            T max = elements[0];
+
+            // Loop through all elements to find the maximum
+            for (T element : elements) {
+                if (element.compareTo(max) > 0) {
+                    max = element;
+                }
+            }
+
+            return max;
         }
 
-        return max;
-    }
+        // Static generic method to print all elements and the maximum
+        public static <T> void printMax(T[] elements, T max) {
+            System.out.print("Comparing values: ");
+            for (T element : elements) {
+                System.out.print(element + " ");
+            }
+            System.out.println("\nMaximum value is: " + max);
+        }
 
     public static void main(String[] args) {
         // Test cases using the generic class
@@ -47,6 +59,8 @@ class MaxInteger<T extends Comparable<T>> {
         // String Test
         MaxInteger<String> stringFinder = new MaxInteger<>("Apple", "Peach", "Banana");
         System.out.println("Max of (Apple, Peach, Banana) is: " + stringFinder.testMaximum());
+
+
     }
 }
 
--- src/test/java/com/bridgelabz/genericsproblems/MaxIntegerTest.java
+++ src/test/java/com/bridgelabz/genericsproblems/MaxIntegerTest.java
@@ -25,4 +25,25 @@ public class MaxIntegerTest {
         String expectedMax = "Peach"; // Alphabetically "Peach" is the greatest
         assertEquals(expectedMax, stringFinder.testMaximum());
     }
+
+    // Test case for Integer with more than three elements
+    @Test
+    public void WhenGivenMoreThanThreeIntegers_ShouldReturnMax_Integer() {
+        MaxInteger<Integer> maxNum = new MaxInteger<>(10, 20, 5, 30, 15);
+        assertEquals(30, maxNum.testMaximum(), "Max should be 30");
+    }
+
+    // Test case for Float with more than three elements
+    @Test
+    public void WhenGivenMoreThanThreeFloats_ShouldReturnMax_Float() {
+        MaxInteger<Float> maxNum = new MaxInteger<>(5.5f, 10.5f, 9.9f, 15.6f, 12.2f);
+        assertEquals(15.6f, maxNum.testMaximum(), "Max should be 15.6");
+    }
+
+    // Test case for String with more than three elements
+    @Test
+    public void WhenGivenMoreThanThreeStrings_ShouldReturnMax_String() {
+        MaxInteger<String> maxNum = new MaxInteger<>("Apple", "Peach", "Banana", "Zebra", "Lion");
+        assertEquals("Zebra", maxNum.testMaximum(), "Max should be Zebra");
+    }
 }
