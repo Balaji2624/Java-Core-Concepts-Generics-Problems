151a8db69aad15869fda26fd4c292181fdb61b8b
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-21 11:05:49 +0530

    Added: Test Max Number at Second position return the same number.

--- src/main/java/com/bridgelabz/genericsproblems/MaxInteger.java
+++ src/main/java/com/bridgelabz/genericsproblems/MaxInteger.java
@@ -23,6 +23,9 @@ class MaxInteger {
         Integer a1 = 9, b1 = 3, c1 = 5;
         System.out.println("Max of (9, 3, 5) is: " + findMax(a1, b1, c1));
 
+        Integer a2 = 3, b2 = 12, c2 = 7;
+        System.out.println("Max of (3, 12, 7) is: " + findMax(a2, b2, c2));
+
     }
 }
 
--- src/test/java/com/bridgelabz/genericsproblems/MaxIntegerTest.java
+++ src/test/java/com/bridgelabz/genericsproblems/MaxIntegerTest.java
@@ -10,4 +10,11 @@ public class MaxIntegerTest {
         Integer a = 10, b = 5, c = 7;
         assertEquals(a, MaxInteger.findMax(a, b, c), "Max should be at the first position");
     }
+
+    //TC 1.2:
+    @Test
+    public void findMax_whenMaxIsAtSecondPosition_shouldReturnSecondValue() {
+        Integer a = 3, b = 12, c = 7;
+        assertEquals(b, MaxInteger.findMax(a, b, c), "Max should be at the second position");
+    }
 }
