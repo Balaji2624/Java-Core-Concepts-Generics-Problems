eaaf61bdd6649b45a373d7e357bd1d07b1858703
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-21 12:23:48 +0530

    Added: Test max Number at second position return the same number.

--- src/main/java/com/bridgelabz/genericsproblems/MaxInteger.java
+++ src/main/java/com/bridgelabz/genericsproblems/MaxInteger.java
@@ -21,6 +21,9 @@ class MaxInteger {
         float a1 = 9.1f, b1 = 3.2f, c1 = 5.2f;
         System.out.println("Max of (9.1, 3.2, 5.2) is: " + findMax(a1, b1, c1));
 
+        Float a2 = 3.5f, b2 = 12.6f, c2 = 7.4f;
+        System.out.println("Max of (3.5, 12.6, 7.4) is: " + findMax(a2, b2, c2));
+
     }
 }
 
--- src/test/java/com/bridgelabz/genericsproblems/MaxIntegerTest.java
+++ src/test/java/com/bridgelabz/genericsproblems/MaxIntegerTest.java
@@ -6,30 +6,37 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MaxIntegerTest {
     // TC 1.1 :
-//    @Test
-//    public void findMax_whenMaxIsAtFirstPosition_shouldReturnFirstValue() {
-//        Integer a = 10, b = 5, c = 7;
-//        assertEquals(a, MaxInteger.findMax(a, b, c), "Max should be at the first position");
-//    }
-//
-//    //TC 1.2:
-//    @Test
-//    public void findMax_whenMaxIsAtSecondPosition_shouldReturnSecondValue() {
-//        Integer a = 3, b = 12, c = 7;
-//        assertEquals(b, MaxInteger.findMax(a, b, c), "Max should be at the second position");
-//    }
-//
-//    //TC 1.3 :
-//    @Test
-//    public void findMax_whenMaxIsAtThirdPosition_shouldReturnThirdValue() {
-//        Integer a = 2, b = 4, c = 10;
-//        assertEquals(c, MaxInteger.findMax(a, b, c), "Max should be at the third position");
-//    }
+    @Test
+    public void findMax_whenMaxIsAtFirstPosition_shouldReturnFirstValue() {
+        Integer a = 10, b = 5, c = 7;
+        assertEquals(a, MaxInteger.findMax(a, b, c), "Max should be at the first position");
+    }
+
+    //TC 1.2:
+    @Test
+    public void findMax_whenMaxIsAtSecondPosition_shouldReturnSecondValue() {
+        Integer a = 3, b = 12, c = 7;
+        assertEquals(b, MaxInteger.findMax(a, b, c), "Max should be at the second position");
+    }
+
+    //TC 1.3 :
+    @Test
+    public void findMax_whenMaxIsAtThirdPosition_shouldReturnThirdValue() {
+        Integer a = 2, b = 4, c = 10;
+        assertEquals(c, MaxInteger.findMax(a, b, c), "Max should be at the third position");
+    }
 
     // TC 2.1 :
     @Test
     public void findMax_whenMaxIsAtFirstPosition_shouldReturnFirstValue() {
         Float a = 9.5f, b = 3.2f, c = 5.7f;
         assertEquals(a, MaxInteger.findMax(a, b, c), "Max should be at the first position");
+    }
+
+    // TC 2.2 :
+    @Test
+    public void findMax_whenMaxIsAtSecondPosition_shouldReturnSecondValue() {
+        Float a = 3.5f, b = 12.6f, c = 7.4f;
+        assertEquals(b, MaxInteger.findMax(a, b, c), "Max should be at the second position");
     }
 }
