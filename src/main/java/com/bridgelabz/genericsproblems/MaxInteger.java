a535a8ef5d429b3b772a027d882dcfbc32b15b48
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-21 12:46:30 +0530

    Added: find maximum from a the String.

--- src/main/java/com/bridgelabz/genericsproblems/MaxInteger.java
+++ src/main/java/com/bridgelabz/genericsproblems/MaxInteger.java
@@ -16,6 +16,21 @@ class MaxInteger {
             return max;
         }
 
+        // Find Maximium String
+        public static String findMax(String a, String b, String c) {
+            String max = a;
+
+            if (b.compareTo(max) > 0) {
+                max = b;
+            }
+
+            if (c.compareTo(max) > 0) {
+                max = c;
+            }
+
+            return max;
+        }
+
     public static void main(String[] args) {
 
         float a1 = 9.1f, b1 = 3.2f, c1 = 5.2f;
@@ -26,6 +41,10 @@ class MaxInteger {
 
         Float a3 = 2.9f, b3 = 4.8f, c3 = 10.2f;
         System.out.println("Max of (2.9, 4.8, 10.2) is: " + findMax(a3, b3, c3));
+
+        // String
+        String a4 = "Apple", b4 = "Peach", c4 = "Banana";
+        System.out.println("Max of (Apple, Peach, Banana) is: " + findMax(a4, b4, c4));
 
     }
 }
