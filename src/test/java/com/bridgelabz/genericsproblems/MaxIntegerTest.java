ec739a94acf171cd8ac3610ec0bf37e26c8d56e4
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-21 10:53:31 +0530

    Added: Test given max number at 1st position return the same number.

--- src/test/java/com/bridgelabz/genericsproblems/MaxIntegerTest.java
+++ src/test/java/com/bridgelabz/genericsproblems/MaxIntegerTest.java
@@ -1,4 +1,13 @@
 package com.bridgelabz.genericsproblems;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MaxIntegerTest {
+    @Test
+    public void findMax_whenMaxIsAtFirstPosition_shouldReturnFirstValue() {
+        Integer a = 10, b = 5, c = 7;
+        assertEquals(a, MaxInteger.findMax(a, b, c), "Max should be at the first position");
+    }
 }
